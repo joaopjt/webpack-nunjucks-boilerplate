f3df5f974442f49e477e76d60c1e47aff85ec1c8
John <joaopjt@gmail.com>
2020-06-17 17:28:46 -0700

    Adding lighter version with laravel-mix

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,12 @@
 require('babel-polyfill');
 
 const webpack = require('webpack');
+const mix = require('laravel-mix');
 const path = require('path');
 const glob = require('glob');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const StyleLintPlugin = require('stylelint-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const autoPrefixer = require('autoprefixer');
 
 const isDev = (process.env.NODE_ENV === 'development') ? true : false;
 const basePath = process.cwd();
@@ -31,73 +30,14 @@ const pages = glob.sync('**/*.njk', {
   template: `resources/html/pages/${page}`,
 }));
 
+mix
+  .js('./resources/assets/js/index.js', 'assets/js/bundle.js')
+  .sass('./resources/assets/scss/main.scss', 'assets/css')
+  .setPublicPath('dist');
 
 module.exports = {
-  entry: {
-    app: [
-      'babel-polyfill',
-      './resources/assets/js/index.js',
-      './resources/assets/scss/main.scss'
-    ]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-        options: {
-          emitError: true,
-          emitWarning: true
-        },
-        enforce: 'pre'
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.s[ac]ss/,
-        use: ExtractTextPlugin.extract({
-          use: [
-            { 
-              loader: "css-loader",
-              options: {
-                url: false
-              }
-            },
-            {
-              loader: "postcss-loader"
-            },
-            {
-              loader: "sass-loader"
-            }
-          ],
-          fallback: "style-loader"
-        })
-      },
-      {
-        test: /\.(njk|nunjucks)$/,
-        loader: ['html-loader', `nunjucks-html-loader?${nunjucksOptions}`]
-      },
-      {
-        test: /\.(jpe?g|png|svg|gif)$/,
-        loader: 'file-loader',
-        options: {
-          name: (isDev) ? '[name].[ext]' : 'assets/images/[name].[hash:8].[ext]'
-        }
-      }
-    ]
-  },
-  output: {
-    path: basePath + '/dist',
-    filename: 'assets/js/bundle.js'
-  },
   plugins: [
-    ...pages,
-    new StyleLintPlugin({ syntax: 'scss' }),
-    new ExtractTextPlugin('assets/css/main.css')
+    ...pages
   ],
   devServer: {
     contentBase: basePath + '/resources',
